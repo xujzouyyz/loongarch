884fcffe8782b4cf3ad75c535771ecc899e83d59
xujzouyyz <xujz21@mails.tsinghua.edu.cn>
2024-07-11 19:07:22 +0800

    init

---
+++ libc/src/stdlib/exit.cpp
@@ -0,0 +1,22 @@
+//===-- Implementation of exit --------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "src/stdlib/exit.h"
+#include "src/__support/OSUtil/exit.h"
+#include "src/__support/common.h"
+
+namespace LIBC_NAMESPACE {
+
+extern "C" void __cxa_finalize(void *);
+
+[[noreturn]] LLVM_LIBC_FUNCTION(void, exit, (int status)) {
+  __cxa_finalize(nullptr);
+  internal::exit(status);
+}
+
+} // namespace LIBC_NAMESPACE
